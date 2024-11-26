5b5765e100df395636441d9dae5e950e090c2ff6
LuNapo12 <145813834+LuNapo12@users.noreply.github.com>
2024-11-26 10:15:02 -0300

    Add files via upload

---
+++ lucas 2 nem a/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem é o maior ganhador do Ballon Door?',
+    'O maior ganhador é Lionel Messi, com 8 premiações.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Bélgica?',
+    'A capital da Bélgica é Bruxelas.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
